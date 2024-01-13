fea4284aba6df973faf3726a455b18af85fa23a0
Pentacosiarca <Pentacosiarca@users.noreply.github.com>
2024-01-13 05:26:28 +0100

    puppeteer

---
+++ src/app_puppetier.js
@@ -0,0 +1,9 @@
+import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web/puppeteer";
+
+/**
+ * Loader uses `page.evaluate(() => document.body.innerHTML)`
+ * as default evaluate function
+ **/
+const loader = new PuppeteerWebBaseLoader("https://www.tabnews.com.br/");
+
+const docs = await loader.load();
